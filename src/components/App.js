f39e69be9ac772d83d2868ee812e88ddbe49f130
zkhan <zkhan@nine.com.au>
2019-06-24 17:18:52 +1000

    List courses

--- src/components/App.js
+++ src/components/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import HomePage from "./HomePage";
 import AboutPage from "./AboutPage";
 import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
 
 function App() {
   function getPage() {
     const route = window.location.pathname;
     console.log(route);
     if (route === "/about") return <AboutPage />;
+    if (route === "/courses") return <CoursesPage />;
     else return <HomePage />;
   }
   return (
---
+++ src/components/CoursesPage.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { getCourses } from "../api/courseApi";
+
+class CoursesPage extends React.Component {
+  state = {
+    courses: []
+  };
+
+  componentDidMount() {
+    getCourses().then(courses => this.setState({ courses: courses }));
+  }
+
+  renderRow(course) {
+    return (
+      <tr>
+        <td>{course.title}</td>
+        <td>{course.authorId}</td>
+        <td>{course.category}</td>
+      </tr>
+    );
+  }
+  render() {
+    return (
+      <>
+        <h2>Courses</h2>
+        <table className="table">
+          <thead>
+            <th>Title</th>
+            <th>Author</th>
+            <th>Category</th>
+          </thead>
+          <tbody>{this.state.courses.map(this.renderRow)}</tbody>
+        </table>
+      </>
+    );
+  }
+}
+
+export default CoursesPage;
