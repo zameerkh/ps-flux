fdcd5b0f12ded845a834394d32d688d0bc3f5520
zkhan <zkhan@nine.com.au>
2019-06-24 15:10:23 +1000

    Added start page

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function HomePage() {
+  return (
+    <div className="jumbotron">
+      <h1>Pluralsight administration</h1>
+      <p>React, Flux and React Router for ultra responsice web apps</p>
+    </div>
+  );
+}
+
+export default HomePage;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import React from "react";
+import { render } from "react-dom";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
+render(<AboutPage />, document.getElementById("root"));
