4e8c997286f03d8cc4d33b0de407ef35b92b66f7
zameer khan <zameer.kh@gmail.com>
2019-06-30 08:41:55 +1000

    Fix issues with not found page

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,7 @@ import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
 import NotFoundPage from "./NotFoundPage";
-import { Route, Switch  } from "react-router-dom";
+import { Route, Switch, Redirect  } from "react-router-dom";
 
 
 function App() {
@@ -15,6 +15,7 @@ function App() {
     <Route path="/" exact component={HomePage}  ></Route>
     <Route path="/courses" component={CoursesPage}></Route>
     <Route path="/about" component={AboutPage}></Route>
+    <Redirect from="/about-page" to="about"></Redirect>
     <Route  component={NotFoundPage}></Route>
     </Switch>
 
--- src/components/NotFoundPage.js
+++ src/components/NotFoundPage.js
@@ -1,15 +1,17 @@
-import { React } from "react";
+import  React  from "react";
 import { Link } from "react-router-dom";
 
-function NotFoundPage(){
-    return (
+function NotFoundPage() {
+  return (
+      <>
     <div>
-    <h2>Page Not Found</h2>
-    <p>
-    <Link to="/" >Back to Home</Link>
-    </p>
+      <h2>Page Not Found</h2>
+      <p>
+        <Link to="/">Back to Home</Link>
+      </p>
     </div>
-    );
+    </>
+  );
 }
 
 export default NotFoundPage;
