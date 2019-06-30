67afa6c2d18f498affb5ddd920c326cca4d81643
zkhan <zkhan@nine.com.au>
2019-06-30 11:53:31 +1000

    Add manage course page

--- src/components/CoursesPage.js
+++ src/components/CoursesPage.js
@@ -1,22 +1,23 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { getCourses } from "../api/courseApi";
 import CoursesList from "./CoursesList";
+import { Link } from "react-router-dom";
 
 function CoursesPage() {
-  const [courses, setCourses] = useState([])
-useEffect(()=>{
-  getCourses().then(_courses => setCourses(_courses))
-}, [])
+  const [courses, setCourses] = useState([]);
+  useEffect(() => {
+    getCourses().then(_courses => setCourses(_courses));
+  }, []);
 
-
-
-    return (
-      <>
-        <h2>Courses</h2>
-        <CoursesList courses={courses}></CoursesList>
-      </>
-    );
-  }
-
+  return (
+    <>
+      <h2>Courses</h2>
+      <Link className="btn btn-primary" to="/course">
+        Add course
+      </Link>
+      <CoursesList courses={courses} />
+    </>
+  );
+}
 
 export default CoursesPage;
--- src/components/ManageCoursePage.js
+++ src/components/ManageCoursePage.js
@@ -1,11 +1,13 @@
 import React from "react";
+import CourseForm from "./CourseForm";
 import { Prompt } from "react-router-dom";
 const ManageCoursePage = props => {
   return (
     <>
       <h2>Manage Course</h2>
+      <CourseForm />
       {/* <Prompt when={true} message="Are you sure you want to leave?" /> */}
-      {props.match.params.slug}
+      {/* {props.match.params.slug} */}
     </>
   );
 };
