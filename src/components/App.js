e955b108f7f0360a3e5cddda8df1b2944a9bc534
zkhan <zkhan@nine.com.au>
2019-06-30 11:35:49 +1000

    Completing react router, slug etc

--- src/components/App.js
+++ src/components/App.js
@@ -4,25 +4,23 @@ import AboutPage from "./AboutPage";
 import Header from "./common/Header";
 import CoursesPage from "./CoursesPage";
 import NotFoundPage from "./NotFoundPage";
-import { Route, Switch, Redirect  } from "react-router-dom";
-
+import { Route, Switch, Redirect } from "react-router-dom";
+import ManageCoursePage from "./ManageCoursePage";
 
 function App() {
- return (
-   <div className="container-fluid"> 
-   <Header></Header>
-    <Switch>
-    <Route path="/" exact component={HomePage}  ></Route>
-    <Route path="/courses" component={CoursesPage}></Route>
-    <Route path="/about" component={AboutPage}></Route>
-    <Redirect from="/about-page" to="about"></Redirect>
-    <Route  component={NotFoundPage}></Route>
-    </Switch>
-
-   </div>
- );
-    
-
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/courses" component={CoursesPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Redirect from="/about-page" to="about" />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
+  );
 }
 
 export default App;
--- src/components/CoursesList.js
+++ src/components/CoursesList.js
@@ -1,39 +1,40 @@
-import React from 'react'
-import PropTypes from 'prop-types';
-
-function CoursesList(props)
-{
-    
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+function CoursesList(props) {
   function renderRow(course) {
     return (
       <tr key={course.id}>
-        <td>{course.title}</td>
+        <td>
+          <Link to={"/course/" + course.slug}> {course.title}</Link>
+        </td>
         <td>{course.authorId}</td>
         <td>{course.category}</td>
       </tr>
     );
   }
-    return(
-        <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Author</th>
-            <th>Category</th>
-          </tr>
-        </thead>
-        <tbody>{props.courses.map(renderRow)}</tbody>
-      </table>
-    )
+  return (
+    <table className="table">
+      <thead>
+        <tr>
+          <th>Title</th>
+          <th>Author</th>
+          <th>Category</th>
+        </tr>
+      </thead>
+      <tbody>{props.courses.map(renderRow)}</tbody>
+    </table>
+  );
 }
 
 CoursesList.propTypes = {
-    courses: PropTypes.arrayOf(PropTypes.shape({
-        id: PropTypes.number.isRequired,
-        title : PropTypes.string.isRequired,
-        authorId : PropTypes.number.isRequired,
-        category: PropTypes.string.isRequired
-
-    })).isRequired
-}
-export default CoursesList
+  courses: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      title: PropTypes.string.isRequired,
+      authorId: PropTypes.number.isRequired,
+      category: PropTypes.string.isRequired
+    })
+  ).isRequired
+};
+export default CoursesList;
